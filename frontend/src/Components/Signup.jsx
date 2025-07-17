97c485742cd7c7d574cd87c8ceb72bda49a4a0d9
Nischay Kumar <iamnischay007@gmail.com>
2025-07-17 14:20:28 +0530

    Update Signup.jsx

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
             return handleError('name, email and password are required')
         }
         try{
-             const url = `http://localhost:8080/auth/signup`;
+             const url = `https://task-tracking-system-1.onrender.com/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
